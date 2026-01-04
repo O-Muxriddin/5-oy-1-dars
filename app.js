8c1700ba85dee0955806a79853c9eb992281c45e
Muhriddin <muhriddin@mozej.com>
2026-01-04 11:39:30 +0500

    change3

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ fetch("https://json-api.uz/api/project/fn44-amaliyot/cars")
         });
     }
 )
+.catch(err => {
+  showError("405: Method Not Allowed");
+});
 .finally(
     (res)=>{
         uiWrite(elementsArray);
